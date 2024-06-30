b63c58b0c739b5b710f8cd2bd3c0773018e014c8
Jihad Ballout <148473573+yeezySznApproaching@users.noreply.github.com>
2024-06-30 14:42:21 +0200

    created a core service repository which i initialized a nodejs project in, installed all the required dependencies, and created a core service file index.js

---
+++ core-service/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const services = {
+    user: 'http://localhost:3001',
+    room: 'http://localhost:3002',
+    booking: 'http://localhost:3003'
+}
+
+app.use('/api/:service/:endpoint', async (req, res) => {
+    const {service, endpoint} = req.params;
+    const url = `${services[service]}/${endpoint}`;
+
+    try {
+        const response = await axios ({
+            method: req.method,
+            url: url,
+            data: req.body
+        });
+
+        res.status(response.status).json(response.data);
+    }catch (error){
+        res.status(500).json({ error: 'Service is not available'});
+    }
+});
+
+app.listen(PORT, ()=>{
+    console.log(`API Gateway is running on port ${PORT}`);
+});
